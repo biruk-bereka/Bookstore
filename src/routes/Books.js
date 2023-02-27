9e6156d27e33870264f7a6a47b037032b562ab74
biruk-bereka <birukbeletech1212@gmail.com>
2023-02-28 00:33:52 +0300

    Display books received from booksSlice

--- src/routes/Books.js
+++ src/routes/Books.js
@@ -1,29 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from '../components/Book';
 import Form from '../components/Form';
 
 const Books = () => {
-  const books = [
-    {
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
-      category: 'Action',
-    },
-    {
-      title: 'Dune',
-      author: 'Frank Herbert',
-      category: 'Science Fiction',
-    },
-    {
-      title: 'Cappital the Tewnty-First Century',
-      author: 'Suzanne Collins',
-      category: 'Economy',
-    },
-  ];
+  const { books } = useSelector((store) => store.books);
+
   return (
     <div>
       {books.map((book) => (
-        <Book key={book.title} title={book.title} author={book.author} category={book.category} />
+        <Book key={book.item_id} title={book.title} author={book.author} category={book.category} />
       ))}
       <Form />
     </div>

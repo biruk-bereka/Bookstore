d8df9244f2500803b7ae5bbfeae25b6f827ba5fb
biruk-bereka <birukbeletech1212@gmail.com>
2023-02-27 21:35:07 +0300

    Configured a redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import { Provider } from 'react-redux';
 import Bookstore from './components/Bookstore';
+import store from './redux/store';
 
 function App() {
   return (
     <BrowserRouter>
-      <Bookstore />
+      <Provider store={store}>
+        <Bookstore />
+      </Provider>
     </BrowserRouter>
   );
 }

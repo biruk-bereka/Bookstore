15eb0bc78cc9bacf50da1bb7b5d95be8cfc9a9bf
biruk-bereka <birukbeletech1212@gmail.com>
2023-02-27 22:18:40 +0300

    Create categories slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/bookSlice';
+import categoriesReducer from './categories/categoriesSlice';
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
+    categories: categoriesReducer,
   },
 });
 
